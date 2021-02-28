8149181585720bcf6ad24cee9ee28464270636e8
ifeanyimuogbo <doc3coder@gmail.com>
2021-02-28 02:42:56 +0100

    done

---
+++ data.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    url:
+      "https://v39-us.tiktokcdn.com/482f5400a3ce5a05fc4ac2ef997ce27b/603ad115/video/tos/useast2a/tos-useast2a-ve-0068c004/07e95e9f8203423882d7e1f8e08ba837/?a=1233&br=1938&bt=969&cd=0%7C0%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=6&er=&l=20210227170849010190219210023ACAD6&lr=&mime_type=video_mp4&pl=0&qs=4&rc=anF0bXBvNGY2MzMzOzczM0ApaDZmZ2VnNGQ6N2Q5ODU2OmdkL2kyNTJjMm9gLS1iMTZzczReLTJeYGExYy9jYzEtMGA6Yw%3D%3D&vl=&vr=",
+    channel: "ifeanyi__",
+    description: "Wow! This is amazing",
+    song: "a byzantine lullaby",
+    likes: 111,
+    comments: 222,
+    shares: 333,
+  },
+  {
+    url:
+      "https://v77.tiktokcdn.com/7a8c4f465eabc1eb34b26c0a96ec8a1b/603b09d7/video/tos/useast2a/tos-useast2a-ve-0068c002/b15fdb0afdf549b8889729734e95297c/?a=1233&br=1204&bt=602&cd=0%7C0%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=6&er=&l=2021022721110401018907221655534103&lr=&mime_type=video_mp4&pl=0&qs=4&rc=MzRoM3JzZHc0MzMzaTczM0ApNDpoZGdoZGU8N2Y8NjQ1aWdpcHIyMmhqbXFgLS1iMTZzczU2NWM1XjUvMzZeLTYwLjA6Yw%3D%3D&vl=&vr=",
+    channel: "ifeanyi__",
+    description: "Wow! This is amazing",
+    song: "a byzantine lullaby",
+    likes: 111,
+    comments: 222,
+    shares: 333,
+  },
+  {
+    url:
+      "https://v77.tiktokcdn.com/a2efc4ed6708de67101b8ec970138317/603b236f/video/tos/useast2a/tos-useast2a-ve-0068c003/e1b9cc8bac6645aa8776488b1ca31c7e/?a=1233&br=1582&bt=791&cd=0%7C0%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=6&er=&l=202102272259320101880612273B5C6A51&lr=&mime_type=video_mp4&pl=0&qs=4&rc=M25ycWx1NnM8MzMzaTczM0ApZmk4ZTQ4aWRoNzVpZmgzOWdeZmotNjYzZHFgLS1jMTZzczViYzUtNmAuYzMuNGMtLWA6Yw%3D%3D&vl=&vr=",
+    channel: "jennifrank29__",
+    description:
+      "i bought this car 15 million Naira🤗😂❤️, argue with your phone",
+    song: "original sound - Jenni_frank",
+    likes: 111,
+    comments: 222,
+    shares: 333,
+  },
+];
+
+export default data;
---
+++ server.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import Data from "./data.js";
+import Videos from "./dbModel.js";
+// config
+const app = express();
+const port = 9000;
+
+// middlewares
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeaders("Access-Control-Allow-Origin", "*");
+  res.setHeaders("Access-Control-Allow-Headers", "*");
+  next();
+});
+// DB config
+const connectionURI =
+  "mongodb+srv://ifeanyi-learn:ifeanyi@cluster0.zgckt.mongodb.net/tiktok-clone?retryWrites=true&w=majority";
+mongoose.connect(connectionURI, {
+  useNewUrlParser: true,
+  useFindAndModify: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+// api endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+app.get("/v1/posts", (req, res) => res.status(200).send(Data));
+
+app.get("/v2/posts", async (req, res) => {
+  try {
+    const AllVideos = await Videos.find();
+    return res.status(200).send(AllVideos);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+app.post("/v2/posts", async (req, res) => {
+  try {
+    const dbVideos = req.body;
+    console.log(req.body);
+    const newVideo = await Videos.create(dbVideos);
+    return res.status(201).send(newVideo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
